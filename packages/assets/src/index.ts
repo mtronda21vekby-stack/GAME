0bf85adeed1028d4135a6f570f9b927aeb7c47f5
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-15 18:28:27 -0500

    Update index.ts

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,6 +1,8 @@
 import "./tokens.css";
 import "./themes/appleGlass.css";
 export { Icons } from "./icons/index";
+export { HeroArt } from "./heroArt";
+
 export const PwaManifests = {
   site: "/manifest.webmanifest",
   lobby: "/manifest.webmanifest",
