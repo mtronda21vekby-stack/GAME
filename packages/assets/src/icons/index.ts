3a9129ac1df388a5c4c79a954a387e6e266f1dc8
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-15 00:36:53 -0500

    Add files via upload

---
+++ packages/assets/src/icons/index.ts
@@ -0,0 +1,9 @@
+export const Icons = {
+  play: `/icons/ui/play.svg`,
+  close: `/icons/ui/close.svg`,
+  chat: `/icons/ui/chat.svg`,
+  settings: `/icons/ui/settings.svg`,
+  soundOff: `/icons/ui/soundOff.svg`,
+  soundOn: `/icons/ui/soundOn.svg`,
+  crown: `/icons/brand/crown.svg`,
+} as const;
