7906458245af4c8727ca8886fefa9970fc2552ed
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-15 00:36:21 -0500

    Add files via upload

---
+++ packages/assets/src/index.ts
@@ -0,0 +1,7 @@
+import "./tokens.css";
+import "./themes/appleGlass.css";
+export { Icons } from "./icons/index";
+export const PwaManifests = {
+  site: "/manifest.webmanifest",
+  lobby: "/manifest.webmanifest",
+} as const;
