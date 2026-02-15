1453d7e7e62910d9d343e7f73d15714778bd3393
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-15 18:40:57 -0500

    Update main.tsx

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@blackcrown/assets";
 import "@blackcrown/ui";
 import "./app.css";
+import "./styles/site.css";
 import { App } from "./routes/Home";
 import { registerSW } from "./pwa/registerSW";
 import { ErrorBoundary } from "./ErrorBoundary";
