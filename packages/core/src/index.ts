417265f8212eb921a14bcf384dcad9a01f6a4911
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-16 00:50:21 -0500

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,4 @@ export { getFlags, setFlag, isEnabled } from "./featureFlags";
 export { track, onAnalytics, attachConsoleAnalytics } from "./analytics";
 export { createWS } from "./ws/index";
 export type { WSClient, WSMessage } from "./ws/types";
+export * from "./ws/lobbyClient";
