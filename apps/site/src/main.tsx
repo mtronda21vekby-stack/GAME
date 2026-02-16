a636d28fb2725bf5cec2b5c98cf2aa7f065f2787
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-16 01:17:18 -0500

    Update main.tsx

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -4,16 +4,9 @@ import "@blackcrown/assets";
 import "@blackcrown/ui";
 import "./app.css";
 import "./styles/site.css";
-import { App } from "./App";
+import { App } from "./routes/App";
 import { registerSW } from "./pwa/registerSW";
 import { ErrorBoundary } from "./ErrorBoundary";
-
-function setAppVh() {
-  document.documentElement.style.setProperty("--app-vh", `${window.innerHeight}px`);
-}
-setAppVh();
-window.addEventListener("resize", setAppVh, { passive: true });
-window.addEventListener("orientationchange", setAppVh, { passive: true });
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
