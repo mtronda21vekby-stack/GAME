c3fbfc3986d74a61b9a1d9af9a4c5b2a7aef6a7d
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-16 20:00:59 -0500

    Update App.tsx

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -6,8 +6,8 @@ export function App() {
   return (
     <>
       <MatrixBackground
-        opacity={0.12}
-        speed={1.15}
+        opacity={0.13}
+        speed={0.55}          // в 2 раза медленнее
         density={1.05}
         fontSize={16}
         color="rgba(0, 255, 170, 0.95)"
