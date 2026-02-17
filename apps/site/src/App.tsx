b14e87130d98916f259f363ba89c4dd16b471003
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-02-16 19:53:13 -0500

    Update App.tsx

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import MatrixBackground from "./components/MatrixBackground";
 import { Router } from "./routes/Router";
 
 export function App() {
-  return <Router />;
+  return (
+    <>
+      <MatrixBackground
+        opacity={0.12}
+        speed={1.15}
+        density={1.05}
+        fontSize={16}
+        color="rgba(0, 255, 170, 0.95)"
+        glow
+      />
+      <div className="bc-app-layer">
+        <Router />
+      </div>
+    </>
+  );
 }
 
 export default App;
