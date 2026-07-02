597b917d9287f2f2ae281dfe06a986996d1fe307
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-02 05:36:48 -0400

    Export EvoFish Next save store

--- apps/game/src/evofish-next/index.ts
+++ apps/game/src/evofish-next/index.ts
@@ -3,5 +3,6 @@ export * from "./core/types";
 export * from "./content/forms";
 export * from "./content/skins";
 export * from "./state/skinSaveAdapter";
+export * from "./state/nextSaveStore";
 export * from "./render/SkinPreview";
 export * from "./ui/SkinLab";
