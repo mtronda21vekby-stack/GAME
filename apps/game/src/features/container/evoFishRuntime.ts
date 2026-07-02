670608bbdd0dbb0bab5dc28b37f468f17a32e839
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-02 15:27:31 -0400

    Bump EvoFish runtime version for world map panel

--- apps/game/src/features/container/evoFishRuntime.ts
+++ apps/game/src/features/container/evoFishRuntime.ts
@@ -1,4 +1,4 @@
-export const EVOFISH_VERSION = "v0.00.24 alpha";
+export const EVOFISH_VERSION = "v0.00.25 alpha";
 
 function numberFromText(value: string | null | undefined) {
   const n = Number(String(value || "").replace(/[^0-9.\-]/g, ""));
