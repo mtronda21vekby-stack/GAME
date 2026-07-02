ff487192837fa63ea19885448b22d26a331967e2
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-02 12:24:16 -0400

    Bump EvoFish version for Next Apex layer

--- apps/game/src/features/container/evoFishRuntime.ts
+++ apps/game/src/features/container/evoFishRuntime.ts
@@ -1,4 +1,4 @@
-export const EVOFISH_VERSION = "v0.00.17 alpha";
+export const EVOFISH_VERSION = "v0.00.18 alpha";
 
 function numberFromText(value: string | null | undefined) {
   const n = Number(String(value || "").replace(/[^0-9.\-]/g, ""));
