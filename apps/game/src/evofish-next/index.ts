5193d778076e3759a8f61234409b13e136c2e3f8
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-02 05:29:44 -0400

    Export EvoFish Next Skin Lab modules

--- apps/game/src/evofish-next/index.ts
+++ apps/game/src/evofish-next/index.ts
@@ -1,4 +1,7 @@
+export * from "./version";
 export * from "./core/types";
 export * from "./content/forms";
 export * from "./content/skins";
 export * from "./state/skinSaveAdapter";
+export * from "./render/SkinPreview";
+export * from "./ui/SkinLab";
