1716ac55b8854df16288ac9afebf5060a08429e5
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-02 11:12:36 -0400

    Bump EvoFish version for visible Next launcher

--- apps/game/src/features/container/evoFishRuntime.ts
+++ apps/game/src/features/container/evoFishRuntime.ts
@@ -1,4 +1,4 @@
-export const EVOFISH_VERSION = "v0.00.13 alpha";
+export const EVOFISH_VERSION = "v0.00.14 alpha";
 
 function numberFromText(value: string | null | undefined) {
   const n = Number(String(value || "").replace(/[^0-9.\-]/g, ""));
