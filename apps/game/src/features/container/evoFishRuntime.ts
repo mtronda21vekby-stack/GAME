45cdc31663856f4e18080f91e6d807b569d37a88
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-03 09:58:14 -0400

    Bump EvoFish runtime version for mutation draft

--- apps/game/src/features/container/evoFishRuntime.ts
+++ apps/game/src/features/container/evoFishRuntime.ts
@@ -1,4 +1,4 @@
-export const EVOFISH_VERSION = "v0.00.46 alpha";
+export const EVOFISH_VERSION = "v0.00.47 alpha";
 
 function numberFromText(value: string | null | undefined) {
   const n = Number(String(value || "").replace(/[^0-9.\-]/g, ""));
