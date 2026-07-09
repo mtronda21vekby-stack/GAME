6373ebbe8493810ee7fa7da8a25554a2082a6d9e
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-09 00:30:51 -0400

    Install EvoFish fish orientation patch in play session

--- apps/game/src/evofish-next/ui/NextPlaySession.tsx
+++ apps/game/src/evofish-next/ui/NextPlaySession.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { installFishOrientationPatch } from "../render/fishOrientationPatch";
 import { CraftQuickOverlay } from "./CraftQuickOverlay";
 import { HudPolishOverlay } from "./HudPolishOverlay";
 import { MapShaderOverlay } from "./MapShaderOverlay";
 import { NextPlaytest } from "./NextPlaytest";
 import { RunCompleteOverlay } from "./RunCompleteOverlay";
+
+installFishOrientationPatch();
 
 export function NextPlaySession() {
   return (
