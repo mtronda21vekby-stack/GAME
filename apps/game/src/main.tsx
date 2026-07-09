008e4806378276fc65676ed5d450725945769b4e
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-09 13:11:36 -0400

    Import game interaction lock styles

--- apps/game/src/main.tsx
+++ apps/game/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@blackcrown/assets";
 import "@blackcrown/ui";
 import "./app.css";
+import "./app-interaction-lock.css";
 import { App } from "./routes/App";
 
 createRoot(document.getElementById("root")!).render(
