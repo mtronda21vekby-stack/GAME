7e2aa16d5c2427473b529ee952de3e132a718c38
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-09 01:35:36 -0400

    Use stable renderer setup and fixed map overlay

--- apps/game/src/evofish-next/ui/NextPlaySession.tsx
+++ apps/game/src/evofish-next/ui/NextPlaySession.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { installFishMaterialPatch } from "../render/fishMaterialPatch";
 import { installFishOrientationPatch } from "../render/fishOrientationPatch";
 import { CraftQuickOverlay } from "./CraftQuickOverlay";
 import { FpsCounterOverlay } from "./FpsCounterOverlay";
 import { HudPolishOverlay } from "./HudPolishOverlay";
+import { MapModalFixOverlay } from "./MapModalFixOverlay";
 import { MapShaderOverlay } from "./MapShaderOverlay";
 import { NextPlaytest } from "./NextPlaytest";
 import { RunCompleteOverlay } from "./RunCompleteOverlay";
 
 installFishOrientationPatch();
-installFishMaterialPatch();
 
 export function NextPlaySession() {
   return (
     <>
       <NextPlaytest />
       <MapShaderOverlay />
+      <MapModalFixOverlay />
       <HudPolishOverlay />
       <CraftQuickOverlay />
       <FpsCounterOverlay />
