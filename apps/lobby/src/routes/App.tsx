436e99664e542481c6c2ff8e757be97c3f30eedd
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-07-10 22:03:54 -0400

    Mount daily login rewards in active lobby router app

--- apps/lobby/src/routes/App.tsx
+++ apps/lobby/src/routes/App.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from "react";
-import { Router } from "../router";
-import { Lobby } from "./Lobby";
-import { Leaderboard } from "./Leaderboard";
 import { attachConsoleAnalytics, track } from "@blackcrown/core";
+import { DailyLoginReward } from "../components/DailyLoginReward";
+import { Router } from "../router";
+import { Leaderboard } from "./Leaderboard";
+import { Lobby } from "./Lobby";
 
 export function App() {
   useEffect(() => attachConsoleAnalytics(), []);
   useEffect(() => track({ type: "page_view", path: window.location.pathname }), []);
 
   return (
-    <Router
-      routes={[
-        { path: "/", element: <Lobby /> },
-        { path: "/lobby", element: <Lobby /> },
-        { path: "/leaderboard", element: <Leaderboard /> },
-        { path: "/top", element: <Leaderboard /> },
-        { path: "/game/progress", element: <Leaderboard /> }
-      ]}
-    />
+    <>
+      <Router
+        routes={[
+          { path: "/", element: <Lobby /> },
+          { path: "/lobby", element: <Lobby /> },
+          { path: "/leaderboard", element: <Leaderboard /> },
+          { path: "/top", element: <Leaderboard /> },
+          { path: "/game/progress", element: <Leaderboard /> }
+        ]}
+      />
+      <DailyLoginReward />
+    </>
   );
 }
