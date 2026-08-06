266bfe8aba3ba5b2b42a2dd0d2bda7b2e1041e47
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-06 01:06:04 -0400

    feat(site): load the V3 home composition layer

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -13,6 +13,7 @@ import "./styles/customer-stability.css";
 import "./styles/brand-nexus.css";
 import "./styles/v3-mobile-art-pass.css";
 import "./styles/dock-v2.css";
+import "./styles/home-v3.css";
 import { App } from "./App";
 import { registerSW } from "./pwa/registerSW";
 import { ErrorBoundary } from "./ErrorBoundary";
