e1a90bc02afa1d97e1fd55cb4bdad02efdf95af5
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 22:29:09 -0400

    Expose BlackCrown site deploy version marker

---
+++ functions/api/deploy-version.ts
@@ -0,0 +1,17 @@
+function json(body: unknown, status = 200) {
+  return new Response(JSON.stringify(body), {
+    status,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store",
+    },
+  });
+}
+
+export const onRequestGet: PagesFunction = async () => {
+  return json({
+    ok: true,
+    service: "blackcrown-site",
+    bridgeContract: "blackcrown-account-bridge-v41.1",
+  });
+};
