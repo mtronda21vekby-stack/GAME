c90a4cc7dddcf78061a6d1b5c1434cce60b81009
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-09-06 19:54:17 -0400

    Bootstrap signed guest session in Lobby

--- apps/lobby/src/routes/App.tsx
+++ apps/lobby/src/routes/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { attachConsoleAnalytics, track } from "@blackcrown/core";
+import { attachConsoleAnalytics, ensureGuestSession, track } from "@blackcrown/core";
 import { DailyLoginReward } from "../components/DailyLoginReward";
 import { Router } from "../router";
 import { WorldDock } from "../worlds/WorldDock";
@@ -10,6 +10,9 @@ import { Lobby } from "./Lobby";
 export function App() {
   useEffect(() => attachConsoleAnalytics(), []);
   useEffect(() => track({ type: "page_view", path: window.location.pathname }), []);
+  useEffect(() => {
+    void ensureGuestSession();
+  }, []);
 
   return (
     <>
